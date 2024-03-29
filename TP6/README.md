534214d52bbe5d80e4c2e780c5b10941dbcd36b7
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2024-03-29 18:37:16 +0000

    tpc6

--- TP6/README.md
+++ TP6/README.md
@@ -1,16 +1,98 @@
-Metainformação: Título, Data, Autor, UC;
-Resumo: Lista de parágrafos;
-Lista de resultados: ficheiros.
+Metainformação: App cinema, 29/03/2024, Rafael Picão Ferreira Correia, RPCW;
 
-## TPC6 Cinema et all:
+Resumo: 
+    1. Carregar o dataset no endpoint 'http://epl.di.uminho.pt:7200',
+    no repositório 'http://epl.di.uminho.pt:7200/repositories/cinema2024'
+    2. Construir queires SPARQL para responder às perguntas:
+        
 
-1. Carregar o dataset no endpoint 'http://epl.di.uminho.pt:7200',
-no repositório 'http://epl.di.uminho.pt:7200/repositories/cinema2024'
-2. Construir queires SPARQL para responder às perguntas:
-    - Quantos filmes existem no repositório?
-    - Qual a distribuição de filmes por ano de lançamento?
-    - Qual a distribuição de filmes por género?
-    - Em que filmes participou o ator "Bur Reynolds"?
-    - Produz uma lista de realizadores com o seu nome e o número de filmes que realizou.
-    - Qual o título dos livros que aparecem associados aos filmes?
-3. Usar o flask para gerar uma interface web ao repositório de filmes (obrigatório usar o endpoint definido ao início).
+        ---
+
+        1. Quantos filmes existem no repositório?
+
+        ```sql
+
+        PREFIX : <http://rpcw.di.uminho.pt/2024/cinema/>
+        select (COUNT(?s) as ?nfilmes) where {
+            ?s a :Film .
+        }
+
+        ```
+
+
+        2. Qual a distribuição de filmes por ano de lançamento?
+
+
+
+        ```sql
+        ...
+
+        ```
+
+        3. Qual a distribuição de filmes por género?
+
+
+
+        ```sql
+
+        PREFIX : <http://rpcw.di.uminho.pt/2024/cinema/>
+        select ?genre (COUNT(?s) as ?nfilmes) where {
+            ?s a :Film .
+            ?s :hasGenre ?genre.
+        } 
+        GROUP BY (?genre)
+        ORDER BY DESC (?nfilmes)
+
+        ```
+
+        4. Em que filmes participou o ator "Burt Reynolds"?
+
+
+        ```sql
+
+        PREFIX : <http://rpcw.di.uminho.pt/2024/cinema/>
+        select ?s where {
+            ?s a :Film .
+            ?s :hasActor :Burt_Reynolds.
+        } 
+
+
+        ```
+
+        5. Produz uma lista de realizadores com o seu nome e o número de filmes que realizou.
+
+
+        ```sql
+
+        PREFIX : <http://rpcw.di.uminho.pt/2024/cinema/>
+        select ?director (COUNT(?s) as ?nfilmes) where {
+            ?s a :Film .
+            ?s :hasDirector ?director.
+        } 
+        GROUP BY (?director)
+        ORDER BY DESC (?nfilmes)
+
+
+        ```
+
+        6. Qual o título dos livros que aparecem associados aos filmes?
+
+
+        ```sql
+        ...
+
+        ```
+
+
+
+
+
+
+        ---
+
+    3. Usar o flask para gerar uma interface web ao repositório de filmes (obrigatório usar o endpoint definido ao início).
+
+
+
+Lista de resultados: out.ttl, app_cinema/, script.py.
+
--- TP6/script.py
+++ TP6/script.py
@@ -1,3 +1,4 @@
+import re
 from rdflib import Graph,URIRef,Literal,Namespace
 from rdflib.namespace import RDF,OWL
 import json
@@ -19,22 +20,27 @@ with open("movies.json","r") as f:
     filmes = json.load(f)
     
 for filme in filmes:
+    title = filme["movie"]
+    #make it so if the movie has a [ or a ] it will be replace by an empty string
+    filme["movie"] = filme["movie"].replace("[", "")
+    filme["movie"] = filme["movie"].replace("]", "")
     filme["movie"] = filme["movie"].replace(" ", "_")
     filme["movie"] = filme["movie"].replace("\"", "")
-    filme["movie"] = filme["movie"].replace("[]", "")
     filme["movie"] = filme["movie"].replace("%", "percent")
 
     g.add((URIRef(f"{cinema}{filme["movie"]}"),RDF.type,OWL.NamedIndividual))
     g.add((URIRef(f"{cinema}{filme["movie"]}"),RDF.type,cinema.Film)) 
-    g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.title,Literal(filme["movie"])))
+    g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.title,Literal(title)))
     #description
     if filme["description"]:
         g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.description,Literal(filme["description"][0])))
     #duration, its optional
     if "duration" in filme:
-        g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.duration,Literal(filme["duration"])))
+        g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.duration,Literal(int(filme["duration"]))))
 
     for actor in filme["actors"]:
+        actor = actor.replace("[", "")
+        actor = actor.replace("]", "")
         actor = actor.replace(" ", "_")
         actor = actor.replace("\"", "")
         actor = actor.split("\n")[0]
@@ -43,6 +49,8 @@ for filme in filmes:
         g.add((URIRef(f"{cinema}{actor}"),RDF.type,cinema.Actor)) 
         g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.hasActor,cinema.Film)) 
     for director in filme["directors"]:
+        director = director.replace("[", "")
+        director = director.replace("]", "")
         director = director.replace(" ", "_")
         director = director.replace("\"", "")
         director = director.split("\n")[0]
@@ -50,6 +58,8 @@ for filme in filmes:
         g.add((URIRef(f"{cinema}{director}"),RDF.type,cinema.Director)) 
         g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.hasDirector,cinema.Director))
     for genre in filme["genre"]:
+        genre = genre.replace("[", "")
+        genre = genre.replace("]", "")
         genre = genre.replace(" ", "_")
         genre = genre.replace("\"", "")
         genre = genre.replace("\n", "")
@@ -58,10 +68,23 @@ for filme in filmes:
         g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.hasGenre,cinema.Genre))
     for countrys in filme["country"]:
         countrys = countrys.replace("\n", ",")
-        countries = countrys.split(",\n")
+        #can i split countries with multiple things, for example, it can be with , or with / or with | or with \n
+        delimiter_pattern = r"[,|/\n]"
+
+        # Split the string using the pattern
+        countries = re.split(delimiter_pattern, countrys)
         for country in countries:
+            #[India]
+            #[United_Kingdom]]
+            
+            country = country.replace("[", "")
+            country = country.replace("]", "")
+            country = country.replace("*", "")
             country = country.replace(" ", "_")
             country = country.replace("\"", "") 
+            country = country.replace("_", "")
+            country = country.replace("-", "")
+            country = country.replace(".", "")
             g.add((URIRef(f"{cinema}{country}"),RDF.type,OWL.NamedIndividual))
             g.add((URIRef(f"{cinema}{country}"),RDF.type,cinema.Country)) 
             g.add((URIRef(f"{cinema}{filme["movie"]}"),cinema.hasCountry,cinema.Country))
